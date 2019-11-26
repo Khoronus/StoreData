f84c7cfffe73a7b8e67d7b504e9737553354f11a
Khoronus <antigorite@hotmail.com>
2019-11-26 13:46:00 +0900

    Add:  - recordcontainervideo Class.

---
+++ module/record/inc/record/recordcontainervideo.hpp
@@ -0,0 +1,188 @@
+/**
+* @file recordcontainervideo.hpp
+* @brief Header of the defined class
+*
+* @section LICENSE
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR/AUTHORS BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* @original author Alessandro Moro <alessandromoro.italy@gmail.com>
+* @bug No known bugs.
+* @version 0.2.0.0
+*
+*/
+
+
+#ifndef SENSORCORECORE_SENSORCORECOREWORLD_RECORDCONTAINERVIDEO_HPP__
+#define SENSORCORECORE_SENSORCORECOREWORLD_RECORDCONTAINERVIDEO_HPP__
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <thread>             // std::thread, std::this_thread::yield
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+
+#include <opencv2/opencv.hpp>
+
+#include "buffer/buffer_headers.hpp"
+#include "logger/inc/logger/log.hpp"
+
+#include "record_defines.hpp"
+#include "recordcontainer.hpp"
+#include "codify/codify.hpp"
+#include "storedata_time.hpp"
+
+//#define dWriteData(a, b) try { cv::imwrite(a, b); }                            \
+//                         catch(...) { std::cout << "Default exception"; }
+
+#define dWriteData(a, b) try { 	FILE *fp; \
+                         fp = fopen(a.c_str(), "w"); \
+                         fwrite(b.data, 1, b.size_bytes, fp); \
+                         fclose(fp); }                   \
+                         catch(...) { std::cout << "Default exception"; }
+
+//#define dWriteData(a, b) try { } catch(...){std::cout << "Default exception";}
+
+/** @brief Class to record all the frames currently captured
+*/
+class RecordContainerVideo
+{
+
+public:
+
+	STOREDATA_RECORD_EXPORT RecordContainerVideo();
+
+	/** @brief It push a new frame to save
+	*/
+	STOREDATA_RECORD_EXPORT void push(const std::string &fname, RecordContainerData &rcd);
+
+	/** @brief It push a new microbuffer to save
+	*/
+	STOREDATA_RECORD_EXPORT void push(std::vector<vb::PtrMicrobuffer> &vptr);
+
+	/** @brief It starts the thread
+	*/
+	STOREDATA_RECORD_EXPORT bool start();
+
+	/** @brief It stops the thread
+	*/
+	STOREDATA_RECORD_EXPORT void stop();
+
+	/** @brief Thread used to save the current container of images
+	*/
+	STOREDATA_RECORD_EXPORT void internal_thread();
+
+	/** @brief It sets the save boosting. If true it use multiple threads to
+	save.
+	*/
+	STOREDATA_RECORD_EXPORT void set_save_boost(bool save_boost);
+
+	/** @brief It returns the about size of the writing queue
+	*/
+	STOREDATA_RECORD_EXPORT size_t size_about();
+
+	/** @brief It returns the about size of the microbuffer
+	*/
+	STOREDATA_RECORD_EXPORT size_t size_about_micro();
+
+	/** @brief It returns the running status
+	*/
+	STOREDATA_RECORD_EXPORT bool is_running();
+
+	/** @brief Since the internal thread is detached, it is necessary to 
+	           wait until is not ready.
+
+		The function try to stop for n iterations and wait m ms.
+		@return It returns true in case of success. False otherwise.
+	*/
+	STOREDATA_RECORD_EXPORT bool wait_until_is_not_ready(size_t num_iterations, int sleep_ms);
+
+	/** @brief Since the internal thread is detached, it is necessary to
+		   wait until is not ready.
+
+		The function try to stop for n iterations and wait m ms.
+		@return It returns true in case of success. False otherwise.
+	*/
+	STOREDATA_RECORD_EXPORT bool wait_until_buffer_is_empty(size_t num_iterations, int sleep_ms);
+
+	/** @brief Set the size of the image
+	*/
+	STOREDATA_RECORD_EXPORT void set_size_image(const cv::Size &size_image);
+
+	/** @brief It sets the filename root used to record the data
+	*/
+	STOREDATA_RECORD_EXPORT void set_fname_root(const std::string &fname_root);
+
+private:
+
+	/** @brief Mutex to prevent race condition
+	*/
+	std::mutex mtx_;
+	/** @brief If there is no data to save, it stops
+	*/
+	std::condition_variable cond_;
+	/** @brief Var used to the condition variable
+	*/
+	bool data_ready_ = false;
+	/** @brief Number of threads that are writing (it does not include the
+	class internal thread)
+	*/
+	int num_threads_;
+	/** @brief Maximum numbers of threads that can be run (except the
+	internal thread)
+	*/
+	int max_threads_;
+
+	/** @brief If true it creates as many threads as possible
+	*/
+	bool save_boost_;
+
+	/** @brief If true it continues to save the data
+	*/
+	bool continue_save_;
+	/** @brief If true is running the internal thread
+	*/
+	bool is_running_;
+
+	/** @brief Container with the data to save
+	*/
+	std::queue<std::pair<std::string, RecordContainerData>> container_;
+
+	/** @brief Container with the microbuffer to save
+	*/
+	std::queue<std::vector<vb::PtrMicrobuffer>> container_microbuffer_;
+	/** @brief Approximatively how many elements are under saving for the 
+	           microbuffer.
+	*/
+	size_t num_elems_microbuffer_approx_;
+
+	/** @brief Main video capture
+	*/
+	cv::VideoWriter vw_;
+	std::ofstream fout_;
+
+	/** @brief Size of the image to record
+	*/
+	cv::Size size_image_;
+	/** @brief Name of the video and meta to save
+	*/
+	std::string fname_root_;
+
+	void filevideo_push_frame(cv::VideoWriter &vw, const std::string &fname,
+		cv::Mat &img);
+};
+
+
+
+
+#endif // SENSORCORECORE_SENSORCORECOREWORLD_RECORDCONTAINERVIDEO_HPP__
---
+++ module/record/src/record/recordcontainervideo.cpp
@@ -0,0 +1,199 @@
+/* @file recordcontainervideo.cpp
+ * @brief Main file with the example for the hog descriptor and visualization.
+ *
+ * @section LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR/AUTHORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * @author Alessandro Moro <alessandromoro.italy@gmail.com>
+ * @bug No known bugs.
+ * @version 0.1.0.0
+ * 
+ */
+
+#include "record/inc/record/recordcontainervideo.hpp"
+
+ //-----------------------------------------------------------------------------
+RecordContainerVideo::RecordContainerVideo() {
+	num_threads_ = 0;
+	max_threads_ = 1;
+	is_running_ = false;
+	num_elems_microbuffer_approx_ = 0;
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::push(const std::string &fname, RecordContainerData &rcd) {
+	{
+		std::lock_guard<std::mutex> lk(mtx_);
+		// Add the data
+		container_.push(std::make_pair(fname, rcd));
+		data_ready_ = true;
+	}
+
+	cond_.notify_one();
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::push(std::vector<vb::PtrMicrobuffer> &vptr)
+{
+	{
+		std::lock_guard<std::mutex> lk(mtx_);
+		// Add the data
+		container_microbuffer_.push(vptr);
+		data_ready_ = true;
+	}
+
+	cond_.notify_one();
+}
+//-----------------------------------------------------------------------------
+bool RecordContainerVideo::start() {
+	std::lock_guard<std::mutex> lk(mtx_);
+	if (is_running_) return false;
+	continue_save_ = true;
+	std::thread t1(&RecordContainerVideo::internal_thread, this);
+	t1.detach();
+	return true;
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::stop() {
+	continue_save_ = false;
+	cond_.notify_one();
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::internal_thread() {
+	is_running_ = true;
+	while (continue_save_) {
+
+		// Digest the main buffer frames (it it exist)
+		std::pair<std::string, RecordContainerData> tuple;
+		{
+			std::unique_lock<std::mutex> lk(mtx_);
+			// no data to save
+			if (container_.size() == 0) {
+				data_ready_ = false;
+				//std::cout << "wait: " << data_ready_ << std::endl;
+				cond_.wait(lk, [this] { return data_ready_; });
+				//std::cout << "end wait: " << data_ready_ << std::endl;
+			}
+			// Pop the data
+			// Test if there is data to prevent error due to spurious awake
+			//std::cout << "Container: " << container_.size() << std::endl;
+			if (container_.size() > 0) {
+				tuple = container_.front();
+				container_.pop();
+			}
+		}
+		// Process the data
+		if (tuple.second.data) {
+			// convert the binary in image
+			cv::Mat img = cv::Mat(size_image_, CV_8UC3, tuple.second.data);
+			filevideo_push_frame(vw_, fname_root_ + ".avi", img);
+
+			// open a file with the frame information
+			if (!fout_.is_open()) {
+				fout_.open(fname_root_ + ".txt");
+			}
+			// calculates a timestamp
+			std::string timestamp =
+				storedata::DateTime::get_date_as_string() +
+				" " + std::to_string(cv::getTickCount());
+			fout_ << tuple.first << "|" << timestamp << std::endl;
+
+			//// codify
+			//{
+			//	std::vector< std::string > vmsg, testmsg;
+			//	vmsg.push_back("This is an example");
+			//	vmsg.push_back("of how");
+			//	vmsg.push_back("a data is codified in an image $%&#?!");
+
+			//	// Codify the strings
+			//	int x = data_block_offset, y = data_block_offset;
+			//	storedata::codify::CodifyImage::merge_strings2image(vmsg, data_block_size,
+			//		data_block_offset, m_data_block, x, y);
+
+			//	// combine the images
+			//	cv::Mat m(frame_size, img.type());
+			//	img.copyTo(m(cv::Rect(0, 0, img.cols, img.rows)));
+			//	m_data_block.copyTo(m(cv::Rect(0, img.rows, m_data_block.cols, m_data_block.rows)));
+			//	// record
+			//	filevideo_push_frame(vw_, tuple.first, m);
+			//}
+
+			// Dispose the data
+			tuple.second.dispose();
+		}
+	}
+	is_running_ = false;
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::set_save_boost(bool save_boost) {
+	std::lock_guard<std::mutex> lk(mtx_);
+	save_boost_ = save_boost;
+}
+//-----------------------------------------------------------------------------
+size_t RecordContainerVideo::size_about() {
+	return container_.size() + num_elems_microbuffer_approx_;
+}
+//-----------------------------------------------------------------------------
+size_t RecordContainerVideo::size_about_micro() {
+	return container_microbuffer_.size();
+}
+//-----------------------------------------------------------------------------
+bool RecordContainerVideo::is_running() {
+	return is_running_;
+}
+//-----------------------------------------------------------------------------
+bool RecordContainerVideo::wait_until_is_not_ready(size_t num_iterations, int sleep_ms) {
+	for (size_t i = 0; i < num_iterations; ++i) {
+		if (is_running()) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
+		}
+		else {
+			return true;
+		}
+	}
+	return false;
+}
+//-----------------------------------------------------------------------------
+bool RecordContainerVideo::wait_until_buffer_is_empty(
+	size_t num_iterations, 
+	int sleep_ms) {
+	for (size_t i = 0; i < num_iterations; ++i) {
+		std::cout << "iA: " << i << " " << (size_about() > 0) << " ";
+		if (is_running() && (size_about() > 0)) {
+			std::cout << size_about() << std::endl;
+			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
+		}
+		else {
+			return true;
+		}
+	}
+	return false;
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::set_size_image(const cv::Size &size_image) {
+	size_image_ = size_image;
+}
+//-----------------------------------------------------------------------------
+void RecordContainerVideo::set_fname_root(const std::string &fname_root) {
+	fname_root_ = fname_root;
+	vw_.release();
+	fout_.close();
+	fout_.clear();
+}//-----------------------------------------------------------------------------
+void RecordContainerVideo::filevideo_push_frame(cv::VideoWriter &vw,
+	const std::string &fname, cv::Mat &img) {
+	if (!vw.isOpened()) {
+		vw.open(fname,
+			cv::VideoWriter::fourcc('D', 'I', 'V', 'X'), 30, img.size(),
+			true);
+	}
+	vw << img;
+}
