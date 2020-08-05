6d29aff06dc6453a12fa6e39f988c4bf0f203a3d
Khoronus <antigorite@hotmail.com>
2020-08-05 12:12:33 +0900

    Modify:  - Fix build.

--- configure/win/1. create_slns.bat
+++ configure/win/1. create_slns.bat
@@ -2,7 +2,8 @@ REM location is set with the current called batch location. If different, please
 SET "location=%cd%"
 SET "build_folder=build"
 REM SET "compiler_version=Visual Studio 14 2015 Win64"
-SET "compiler_version=Visual Studio 15 2017 Win64"
+SET "compiler_version=Visual Studio 15 2017"
+SET "compiler_option=x64"
 
 REM Create the C++ solutions
 cd "%location%\..\.."
